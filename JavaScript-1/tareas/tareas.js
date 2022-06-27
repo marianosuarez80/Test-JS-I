b29d963d802536a23ce000218cbed3185c6466f5
marianosuarez80 <ms@MacBook-Pro-de-Mariano.local>
2022-06-27 18:29:13 -0300

    tareas js

--- JavaScript-1/tareas/tareas.js
+++ JavaScript-1/tareas/tareas.js
@@ -8,10 +8,10 @@
 // En estas primeras consignas, reemplaza `null` por la respuesta correcta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "Mariano";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 3;
 
 // Crea una variable booleana:
 const nuevoBool = null;
